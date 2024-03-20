17529511c1ce4daec5d4a5030b902e779f1b84bc
Misha Diagilev <mishadyagilev@gmail.com>
2024-03-20 13:45:34 +0300

    all forms without file

--- internal/handlers.go
+++ internal/handlers.go
@@ -7,8 +7,14 @@ import (
 )
 
 type Cred struct {
-	Username string
-	Password string
+	Username       string
+	Password       string
+	TextareaInput  string
+	OneRadio       string
+	SelectInput    string
+	FileInput      string
+	NotHiddenInput string
+	HiddenInput    string
 }
 
 func login(w http.ResponseWriter, r *http.Request) {
@@ -37,6 +43,19 @@ func proceed(w http.ResponseWriter, r *http.Request) {
 	}
 	username := r.FormValue("username")
 	password := r.FormValue("password")
+	textareaInput := r.FormValue("textareainput")
+	oneRadio := r.FormValue("oneradio")
+	selectinput := r.FormValue("selectinput")
+	nothiddeninput := r.FormValue("titlenothidden")
+	hiddeninput := r.FormValue("postId")
+	//_, fileiInput, err := r.FormFile("file")
+	//if err != nil {
+	//	log.Printf("can not parse file from input:%v", err)
+	//}
+	//fileiInput := r.FormValue("file")
+	//twoRadio := r.FormValue("tworadio")
+	//threeRadio := r.FormValue("threeradio")
+
 	//decode := json.NewDecoder(r.Body)
 	//decode.DisallowUnknownFields()
 	//
@@ -45,7 +64,16 @@ func proceed(w http.ResponseWriter, r *http.Request) {
 	//if err := decode.Decode(&cred); err != nil {
 	//	http.Error(w, "can not decode input data", http.StatusBadRequest)
 	//}
-	var cred = Cred{Username: username, Password: password}
+	var cred = Cred{
+		Username:      username,
+		Password:      password,
+		TextareaInput: textareaInput,
+		OneRadio:      oneRadio,
+		SelectInput:   selectinput,
+		//FileInput:     fileiInput.Filename,
+		NotHiddenInput: nothiddeninput,
+		HiddenInput:    hiddeninput,
+	}
 
 	file, err := template.ParseFiles("cmd/templates/processor.html")
 	if err != nil {
